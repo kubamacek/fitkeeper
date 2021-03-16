3be524ee4e2cd712746a2e7d8df262a192cdc172
kubamacek <kubamacek34@gmail.com>
2021-03-16 21:11:45 +0100

    install and setup cors

--- backend/fitkeeper/settings.py
+++ backend/fitkeeper/settings.py
@@ -39,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'fitkeeper.backendapp',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -124,3 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# CORS settings
+
+CORS_ORIGIN_ALLOW_ALL = True
