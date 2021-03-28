74628c62ff6372de600bdad7e9ca86d38efa8066
kubamacek <kubamacek34@gmail.com>
2021-03-28 19:25:05 +0200

    add activities sections

--- frontend/frontendapp/src/app/sections/foodbase/food.service.ts
+++ frontend/frontendapp/src/app/sections/foodbase/food.service.ts
@@ -1,9 +1,7 @@
 import { Ingredient } from './../../common/interfaces/ingredient.interface';
-import { Response } from './../../common/interfaces/response.interface';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { urls } from 'src/environments/environment';
 
 
 @Injectable({
