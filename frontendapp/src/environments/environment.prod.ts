079164e908c69fc52f11afef07c4227ccd3e2ede
kubamacek <kubamacek34@gmail.com>
2021-05-09 12:58:53 +0200

    deploy frontend to netlify

--- frontendapp/src/environments/environment.prod.ts
+++ frontendapp/src/environments/environment.prod.ts
@@ -1,3 +1,17 @@
 export const environment = {
   production: true
 };
+
+const apiversion = 'https://fitkeeper-backend.herokuapp.com/api/v1/';
+export const urls = {
+  user: apiversion + 'auth-user/',
+  login: apiversion + 'api-token-auth/',
+  refresh: apiversion + 'api-token-refresh/',
+  about: apiversion + 'about/',
+  foodbase: apiversion + 'ingredients/',
+  activities: apiversion + 'activities/',
+  diet: apiversion + 'meals/',
+  trainings: apiversion + 'trainings/',
+  settings: apiversion + 'settings/',
+  dailysummaries: apiversion + 'dailysummaries/'
+};
