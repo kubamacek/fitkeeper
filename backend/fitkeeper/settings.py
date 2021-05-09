425c3f417d615887a709c85a6f87d22878b5ab71
kubamacek <kubamacek34@gmail.com>
2021-05-09 15:00:40 +0200

    configure cors on backend side

--- backend/fitkeeper/settings.py
+++ backend/fitkeeper/settings.py
@@ -160,6 +160,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'fitkeeper/static')
 CORS_ORIGIN_WHITELIST = (
     "http://127.0.0.1:4200",
     "http://localhost:4200",
+    "https://fitkeeper-app.netlify.app",
+    "http://fitkeeper-app.netlify.app"
 )
 
 # REST FRAMEWORK SETTINGS
