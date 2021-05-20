705a7e85606b76acc50702b06d8d388e009bc753
kubamacek <kubamacek34@gmail.com>
2021-05-20 14:14:05 +0200

    use custom docker images

--- frontendapp/karma.conf.js
+++ frontendapp/karma.conf.js
@@ -46,6 +46,6 @@ module.exports = function (config) {
     browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true,
-    browserNoActivityTimeout: 60000
+    browserNoActivityTimeout: 200000
   });
 };
